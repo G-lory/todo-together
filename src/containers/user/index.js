3e437a4ded7cd30c7bd03402fc6b6882ef45ad7d
G-lory <xie_ym@qq.com>
2019-01-15 23:16:23 +0800

    feat: 个人中心页-详细信息

--- src/containers/user/index.js
+++ src/containers/user/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { UserWrapper, UserHeader } from './style';
+import { UserWrapper, UserHeader, UserContent } from './style';
 
 class User extends PureComponent {
 	render() {
@@ -11,8 +11,40 @@ class User extends PureComponent {
 						<p className="username">小夏不姓夏</p>
 						<p className="user-intro">如果人生会有很长，愿有你的荣耀永不散场。</p>
 					</div>
-					
 				</UserHeader>
+				<UserContent>
+					<div className="content-title">
+						用户信息
+					</div>
+					<table className="user-table">
+						<tbody>
+							<tr>
+								<th>姓名</th>
+								<td>夏柯一</td>
+							</tr>
+							<tr>
+								<th>性别</th>
+								<td>女</td>
+							</tr>
+							<tr>
+								<th>邮箱</th>
+								<td>xiaoxiabuxingxia@163.com</td>
+							</tr>
+							<tr>
+								<th>地点</th>
+								<td>北京</td>
+							</tr>
+							<tr>
+								<th>生日</th>
+								<td>1990/11/11</td>
+							</tr>
+							<tr>
+								<th>简介</th>
+								<td>一只废喵~</td>
+							</tr>
+						</tbody>
+					</table>
+				</UserContent>
 			</UserWrapper>
 		)
 	}
--- src/containers/user/style.js
+++ src/containers/user/style.js
@@ -6,17 +6,25 @@ export const UserWrapper = styled.div`
 
 export const UserHeader = styled.div`
 	height: 150px;
-	background: url(${userBg});
-	background-position: -50px -20px;
+// 	background: url(${userBg});
+// 	background-position: -50px -20px;
+	background: #fff;
 	display: flex;
+	box-shadow: 0 1px 11px 0 rgba(0, 0, 0, 0.12);
+	border-radius: 5px;
 	.avatar {
 		height: 100px;
     width: 100px;
     margin: 25px 50px;
     border-radius: 50%;
+		transition: all 1s;
+		box-shadow: 0 0 5px 0 #000;
+		&:hover {
+			transform: scale(1.5);
+		}
 	}
 	.text {
-		color: #fff;
+		color: #555;
 		.username {
 			font-size: 26px;
 			margin-top: 40px;
@@ -25,5 +33,30 @@ export const UserHeader = styled.div`
 		.user-intro {
 			font-size: 16px;
 		}	
+	}
+`;
+
+export const UserContent = styled.div`
+	box-shadow: 0 1px 11px 0 rgba(0, 0, 0, 0.12);
+	margin-top: 20px;
+	padding: 20px;
+	background: #fff;
+	.content-title {
+		height: 50px;
+    font-size: 16px;
+    line-height: 50px;
+    padding-left: 20px;
+    font-weight: 500;
+	}
+	.user-table {
+		width: 100%;
+		th, td {
+			padding: 15px;
+			border-top: 1px solid #e9ecef;	
+		}
+		th {
+			font-weight: 900;
+			width: 200px;
+		}
 	}
 `;
