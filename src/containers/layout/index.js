784408704bbe80d226619f67f98a5c70f0935e0c
G-lory <xie_ym@qq.com>
2019-01-25 00:38:46 +0800

    feat: 尝试使用异步组件 首屏优化

--- src/containers/layout/index.js
+++ src/containers/layout/index.js
@@ -3,10 +3,14 @@ import Menu from './components/Menu';
 import { Route } from 'react-router-dom';
 import Home from '../home';
 import TaskList from '../tasklist';
-import Dashboard from '../dashboard';
 import User from '../user';
 import Forum from '../forum';
 import { LayoutWrapper, Header, Logo, Main } from './style';
+import asyncComponent from 'components/AsyncComponent';
+
+// 尝试引入异步组件 react.lazy 返回一个 function Route 不识别...
+// const Dashboard = lazy(() => import('../dashboard'));
+const AsyncDashboard = asyncComponent(() => import('../dashboard'));
 
 class Layout extends React.PureComponent {
 	render() {
@@ -21,7 +25,7 @@ class Layout extends React.PureComponent {
 				<Main>
 					<Route path='/home' exact component={Home}></Route>
 					<Route path='/tasklist' exact component={TaskList}></Route>
-					<Route path='/dashboard' exact component={Dashboard}></Route>
+					<Route path='/dashboard' exact component={AsyncDashboard}></Route>
 					<Route path='/user' exact component={User}></Route>
 					<Route path='/forum' exact component={Forum}></Route>
 				</Main>
